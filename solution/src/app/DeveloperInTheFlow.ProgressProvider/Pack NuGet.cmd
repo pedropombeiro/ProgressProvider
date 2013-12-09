b4973f41f4af67d25095cb6881762f1f2bc43072
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-12-09 14:39:03 +0100

    Add preparations for automated NuGet publishing.

---
+++ solution/src/app/DeveloperInTheFlow.ProgressProvider/Pack NuGet.cmd
@@ -0,0 +1,2 @@
+..\..\..\.nuget\nuget update -Self
+..\..\..\.nuget\nuget pack -Build -Symbols -Version 1.0.3 -Properties Configuration=Release
--- solution/src/app/DeveloperInTheFlow.ProgressProvider/Properties/AssemblyInfo.cs
+++ solution/src/app/DeveloperInTheFlow.ProgressProvider/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeveloperInTheFlow.ProgressProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides a hierarchical implementation of the System.IProgress&lt;T&gt; interface, allowing for more complex scenarios with multiple parallel operations being reported.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pedro Pombeiro")]
 [assembly: AssemblyProduct("DeveloperInTheFlow.ProgressProvider")]
 [assembly: AssemblyCopyright("Copyright © Pedro Pombeiro 2013")]
 [assembly: AssemblyTrademark("")]
@@ -24,5 +24,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
