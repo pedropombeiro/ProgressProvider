b736230aa262d3fffc881d2fa38ba1ddc50da63d
Pedro Pombeiro <pedropombeiro@gmail.com>
2013-12-09 18:53:17 +0100

    Update to version 1.0.4.

--- solution/src/app/DeveloperInTheFlow.ProgressProvider/Pack NuGet.cmd
+++ solution/src/app/DeveloperInTheFlow.ProgressProvider/Pack NuGet.cmd
@@ -1,2 +1,2 @@
 ..\..\..\.nuget\nuget update -Self
-..\..\..\.nuget\nuget pack -Build -Symbols -Version 1.0.3 -Properties Configuration=Release
+..\..\..\.nuget\nuget pack -Build -Symbols -Version 1.0.4 -Properties Configuration=Release
