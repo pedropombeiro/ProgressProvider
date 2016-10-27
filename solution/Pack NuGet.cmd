c98f53f2833dcf14ab182d1411fe160b2686dfa0
romerod <daniel.romero@baseclass.ch>
2016-10-27 17:18:49 +0200

    Fix concurrency problem when CalculateAggregateProgressReport

--- solution/Pack NuGet.cmd
+++ solution/Pack NuGet.cmd
@@ -1,3 +1,3 @@
 .nuget\nuget update -Self
-.nuget\nuget pack -Build -Symbols -Version 1.0.5 -Properties Configuration=Release
+.nuget\nuget pack -Build -Symbols -Version 1.0.16 -Properties Configuration=Release
 pause
