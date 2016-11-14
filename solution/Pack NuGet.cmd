c5e126f382aca05600a04c15d336b8d5cc0bb5e3
romerod <daniel.romero@baseclass.ch>
2016-11-14 16:43:45 +0100

    Lock access to progress lists, List<> is not thread safe, update versions

--- solution/Pack NuGet.cmd
+++ solution/Pack NuGet.cmd
@@ -1,3 +1,3 @@
 .nuget\nuget update -Self
-.nuget\nuget pack -Build -Symbols -Version 1.0.17 -Properties Configuration=Release
+.nuget\nuget pack -Build -Symbols -Version 1.0.17.4 -Properties Configuration=Release
 pause
--- solution/src/app/DeveloperInTheFlow.ProgressProvider.Net40/Properties/AssemblyInfo.cs
+++ solution/src/app/DeveloperInTheFlow.ProgressProvider.Net40/Properties/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.17")]
-[assembly: AssemblyFileVersion("1.0.17.0")]
+[assembly: AssemblyFileVersion("1.0.17.4")]
